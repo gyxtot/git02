2c9ed5c1ed0a311740a5068c6f8a5e3ac4366054
gaoyuxiang <gyx_1024@163.com>
2017-12-06 17:49:19 +0800

    git02项目初始化

---
+++ src/main/java/com/shsxt/po/User.java
@@ -0,0 +1,25 @@
+package com.shsxt.po;
+
+/**
+ * Created by gaoyuxiang on 2017/12/6.
+ */
+public class User {
+    private Integer id;
+    private String userName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
